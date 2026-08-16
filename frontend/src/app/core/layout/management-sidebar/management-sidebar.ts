c5f9e5f58414ba8be45b4b39e1d3e864f461ec9a
NathanLChamb <36806958+NathanLChamb@users.noreply.github.com>
2026-08-16 17:10:27 -0500

    Begin transition to responsive web design

--- frontend/src/app/core/layout/management-sidebar/management-sidebar.ts
+++ frontend/src/app/core/layout/management-sidebar/management-sidebar.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-management-sidebar',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './management-sidebar.html',
   styleUrl: './management-sidebar.css',
 })
